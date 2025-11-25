b98c216ece06a25874ba557a14b7219b9e28b0d1
minjee <jimin79546@cju.ac.kr>
2025-11-25 18:38:19 +0900

    도서 정보 저장 및 검색 출력 Main class

---
+++ library/src/Bookdata.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class Bookdata {
+    private ArrayList<book> books;
+
+    public Bookdata() {
+        books = new ArrayList<>();
+        // 테스트용 데이터
+        books.add(new book("컴퓨터 구조", "신종홍", "001", "한빛"));
+        books.add(new book("Evolve", "anne", "002", "길벗"));
+        books.add(new book("정의란 무엇인가", "마크 샌더스", "003", "한길"));
+    }
+
+    public void addbook(book book) {
+        books.add(book);
+        System.out.println("추가가 완료되었습니다.");
+    }
+
+    public void serchBytitle(String title) {
+        boolean found = false;
+        for (book book : books) {
+            if (book.getTitle().contains(title)) {
+                book.printInfo();
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("검색 결과가 없습니다.");
+        }
+    }
+
+    public void serchByAuthor(String author) {
+        boolean found = false;
+        for (book book : books) {
+            if (book.getAuthor().contains(author)) {
+                book.printInfo();
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("검색 결과가 없습니다.");
+        }
+    }
+
+    public void serchBycategori(String categori) {
+        boolean found = false;
+        for (book book : books) {
+            if (book.getCode().equals(categori)) {
+                book.printInfo();
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("검색 결과가 없습니다.");
+        }
+    }
+
+    public void addBook(book book) {
+        books.add(book);
+    }
+}
---
+++ library/src/Main.java
@@ -0,0 +1,67 @@
+import java.awt.print.Book;
+import java.util.Scanner;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Bookdata data = new Bookdata();
+
+        while (true) {
+
+            System.out.println("실행할 메뉴를 선택하세요");
+            System.out.println("1.도서 저장");
+            System.out.println("2. 제목 검색");
+            System.out.println("3. 저자 검색");
+            System.out.println("4. 카테고리 검색");
+            System.out.println("0. 종료");
+
+            int main = sc.nextInt();
+            sc.nextLine();
+            switch (main) {
+                case 1:
+                    System.out.println("제목 입력:");
+                    String title = sc.nextLine();
+                    System.out.println("저자 입력:");
+                    String author = sc.nextLine();
+                    System.out.println("카테고리 입력");
+                    String categori = sc.nextLine();
+                    System.out.println("출판사 입력");
+                    String publisher = sc.nextLine();
+
+                    System.out.println("\n===== " + title + " / " + author + " / " + categori + "/" + publisher + " =====");
+                    book newbook = new book(title, author, categori, publisher);
+                    data.addBook(newbook);
+                    break;
+            }
+            switch (main) {
+                case 2:
+                    System.out.println("제목 입력");
+                    String title = sc.nextLine();
+                    data.serchBytitle(title);
+                    break;
+
+                case 3:
+                    System.out.println("저자 입력");
+                    String author = sc.nextLine();
+                    data.serchByAuthor(author);
+                    break;
+
+                case 4:
+                    System.out.println("카테고리 입력");
+                    String categori = sc.nextLine();
+                    data.serchBycategori(categori);
+                    break;
+
+                case 0:
+                    System.out.println("종료 되었습니다.");
+                    return;
+
+                default:
+                    System.out.println("도서 추가가 완료되었습니다.");
+            }
+
+        }
+    }
+}
