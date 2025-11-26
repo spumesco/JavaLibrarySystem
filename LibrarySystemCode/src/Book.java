784a92c496ff0aa331948b8bd2621bd35223022c
조현지 <jjhoyun@gmail.com>
2025-11-26 10:31:28 +0900

    Fix constructor name and update printInfo method
    한국어 깨짐 현상 해결

--- LibrarySystemCode/src/Book.java
+++ LibrarySystemCode/src/Book.java
@@ -3,9 +3,9 @@ public class book {
     private String author;
     private String code;
     private String publisher;
+     
 
-
-
+ 
     public book(String title, String author, String code, String publisher) {
         this.title = title;
         this.author = author;
@@ -13,18 +13,18 @@ public class book {
         this.publisher = publisher;
     }
 
-
+   
     public String getTitle() { return title; }
     public String getAuthor() { return author; }
     public String getCode() { return code; }
     public String getPublisher() {return publisher; }
 
-
+  
     public void printInfo() {
-        System.out.println("�목: " + title);
-        System.out.println("� " + author);
+        System.out.println("제목: " + title);
+        System.out.println("저자: " + author);
         System.out.println("카테고리: " + code);
-        System.out.println("출판 " + publisher);
+        System.out.println("출판사: " + publisher);
         System.out.println("-------------------------");
     }
 }
